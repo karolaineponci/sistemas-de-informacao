d246a0914cb30daf284f9620db6e4500224ab8fc
Karolaine Ponci <laineponci2004@gmail.com>
2025-10-20 20:24:20 -0400

    comentei o exp_listaligada_frutas

--- estrutura de dados/exercicios/exp_listaligada_frutas.py
+++ estrutura de dados/exercicios/exp_listaligada_frutas.py
@@ -1,15 +1,20 @@
-class No:
-    def __init__(self, dado):
-        self.dado = dado # O valor armazenado
-        self.proximo = None # Ponteiro para o próximo nó
+class No: # defnindo a classe No (molde para criar cada elemento da lista)
+    
+    def __init__(self, dado): # método construtor - executado quando criamos um novo objeto da classe
+        self.dado = dado # Atributo dado: guarda o valor que este nó armazena - ex>: Maça, morango...
+
+        self.proximo = None #Atributo proximo: Ponteiro que aponta para o próximo nó (inicialmente não aponta para ninguem - None)
 
 # criando uma lista ligada simples
+# CRIANDO OBJETOS (instâncias da classe No)
+# Cada linha abaixo cria um objeto independente:
 no1 = No("Maçã")
 no2 = No("Melão")
 no3 = No("Melancia")
 no4 = No("Morango")
 
-# Ligando os nós
+# LIGANDO OS OBJETOS PARA FORMAR UMA LISTA ENCADEADA
+# Criando as conexões entre os nós:
 no1.proximo = no2 # Maçã > Melão
 no2.proximo = no3
 no3.proximo = no4
